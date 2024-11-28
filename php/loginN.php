4737eaea7cecc381c225d52ced4c521546cd1165
E8Vitel <vitklalvz@gmail.com>
2024-11-28 15:17:03 -0300

    cambios en login, agregados php para login

---
+++ php/loginN.php
@@ -0,0 +1,27 @@
+<?php
+
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=test_hk', 'root', 'lilian');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error en la conexión: " . $e->getMessage());
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $usuario = $_POST["userN"];
+    $contrasena = $_POST["passN"];
+
+    $query = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND contrasena = '$contrasena'";
+
+    $resultado = $pdo->query($query);
+    $usuario = $resultado->fetch(PDO::FETCH_ASSOC);
+
+    if ($usuario) {
+        session_start();
+        $_SESSION['usuario'] = $usuario['usuario'];
+        header('Location: ../accediste.html');
+    } else {
+        echo "Usuario o contraseña incorrectos.";
+    }
+}
+?>
---
+++ php/loginV.php
@@ -0,0 +1,33 @@
+<?php
+
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=test_hk', 'root', 'lilian');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error en la conexión: " . $e->getMessage());
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Capturar y sanitizar datos del formulario
+    $username = $_POST['userV'];
+    $password = $_POST['passV'];
+
+    // Consulta preparada
+    $sql = "SELECT * FROM usuarios WHERE usuario = :username";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+    $stmt->execute();
+
+    $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // Verificar si se encontró el usuario y si la contraseña coincide
+    if ($usuario) {
+        session_start();
+        $_SESSION['usuario'] = $usuario['usuario'];
+        header('Location: ../accediste.html');
+    } else {
+        echo "Usuario o contraseña incorrectos.";
+
+    }
+}
+?>
